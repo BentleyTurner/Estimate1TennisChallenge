e3e1cc25631eb6171cd53caf3722c0d04dbdd01f
BentleyTurner <bentleyturner@live.com>
2023-04-10 21:29:10 +1000

    Adding Player and Set classes

---
+++ Estimate1TennisChallenge/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estimate1TennisChallenge.Models
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int GamesLost { get; private set; }
+        public int GamesWon { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            GamesLost = 0;
+            GamesWon = 0;
+        }
+
+        public Player(string name, int gamesLost, int gamesWon) : this(name)
+        {
+            GamesLost = gamesLost;
+            GamesWon = gamesWon;
+        }
+
+        public void AddWin() { GamesWon++; }
+        public void AddLoss() { GamesLost++; }
+    }
+}
---
+++ Estimate1TennisChallenge/Models/Set.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estimate1TennisChallenge.Models
+{
+    public class Set
+    {
+        public string Winner { get; }
+        public string Loser { get; }
+
+        public Set(string player1Name, string player2Name)
+        {
+            Winner = player1Name;
+            Loser = player2Name;
+        }
+    }
+}
