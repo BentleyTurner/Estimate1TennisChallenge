b7d25cbfc48b43c33b4c5deedb2d8682c230702f
BentleyTurner <bentleyturner@live.com>
2023-04-10 21:29:47 +1000

    Adding Utils test and building xunit project

---
+++ Estimate1TennisChallengeTest/ProgramTest.cs
@@ -0,0 +1,11 @@
+using Estimate1TennisChallenge;
+using Estimate1TennisChallenge.Models;
+using System.Reflection;
+
+namespace Estimate1TennisChallengeTest
+{
+    public class ProgramTest
+    {
+
+    }
+}
---
+++ Estimate1TennisChallengeTest/UtilsTest.cs
@@ -0,0 +1,43 @@
+﻿using Estimate1TennisChallenge;
+
+namespace Estimate1TennisChallengeTest
+{
+    public class UtilsTest
+    {
+        [Fact]
+        public void ShouldSuccessfullyReturnWhenMatchIdDoesMatch()
+        {
+            string expectedReturn = "Barry defeated Adam\n3 to 2";
+            var actualReturn = Utils.PrintMatchResult("01", Fixtures.testMatchList);
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+
+        [Fact]
+        public void ShouldDisplayErrorWhenMatchIdDoesntMatch()
+        {
+            string expectedReturn = "Couldnt find a match with an ID of: 08";
+            var actualReturn = Utils.PrintMatchResult("08", Fixtures.testMatchList);
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+
+        [Fact]
+        public void ShouldSuccessfullyReturnWhenPlayerNameDoesMatch()
+        {
+            string expectedReturn = "30 15";
+            var actualReturn = Utils.PrintPlayerGames("Garry", Fixtures.testPlayerList);
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+
+        [Fact]
+        public void ShouldDisplayErrorWhenPlayerNameDoesntMatch()
+        {
+            string expectedReturn = "Couldnt find a player with the name: Barry";
+            var actualReturn = Utils.PrintPlayerGames("Barry", Fixtures.testPlayerList);
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+    }
+}
