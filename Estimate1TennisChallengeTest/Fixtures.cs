3089f61ff38a85c5019b09ab6950568382c88a7d
BentleyTurner <bentleyturner@live.com>
2023-04-10 21:28:44 +1000

    Adding Fixtures and Match tests

---
+++ Estimate1TennisChallengeTest/Fixtures.cs
@@ -0,0 +1,22 @@
+﻿using Estimate1TennisChallenge.Models;
+
+namespace Estimate1TennisChallengeTest
+{
+    public static class Fixtures
+    {
+        public static List<Match> testMatchList =
+            new List<Match>() { new Match("01", new Player("Adam"), new Player("Barry"), 2, 3),
+                new Match("02", new Player("Garry"), new Player("Barry"), 1, 3),
+                new Match("03", new Player("Fred"), new Player("Barry"), 3, 1),
+                new Match("04", new Player("Adam"), new Player("Barry"), 0, 3),
+            };
+
+        public static List<Player> testPlayerList = new List<Player>() { new Player("Garry", 15, 30), };
+
+        public static Match testMatchwithPlayer1Winner = new Match("01", new Player("Garry"), new Player("Barry"), 3, 2);
+
+        public static Match testMatchwithPlayer2Winner = new Match("01", new Player("Garry"), new Player("Barry"), 2, 3);
+
+        public static Match testMatchatAtDraw = new Match("01", new Player("Garry"), new Player("Barry"), 2, 2);
+    }
+}
---
+++ Estimate1TennisChallengeTest/MatchTest.cs
@@ -0,0 +1,44 @@
+﻿using Estimate1TennisChallenge;
+
+namespace Estimate1TennisChallengeTest
+{
+    public class MatchTest
+    {
+        [Fact]
+        public void ShouldDisplayPlayer1WinsIfPlayer1has3SetWins()
+        {
+            string expectedReturn = "Garry defeated Barry\n3 to 2";
+            var testMatch = Fixtures.testMatchwithPlayer1Winner;
+            string actualReturn = testMatch.GetMatchResult();
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+
+        [Fact]
+        public void ShouldDisplayPlayer2WinsIfPlayer2has3SetWins()
+        {
+            string expectedReturn = "Barry defeated Garry\n3 to 2";
+            var testMatch = Fixtures.testMatchwithPlayer2Winner;
+
+            string actualReturn = testMatch.GetMatchResult();
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+
+        [Fact]
+        public void ShouldDisplayPlayer1WinsIfAddPointWhenAtMatchPoint()
+        {
+            string expectedReturn = "Garry defeated Barry\n3 to 2";
+            var testMatch = Fixtures.testMatchatAtDraw;
+
+            testMatch.Player1CurrentGameWins = 5;
+            testMatch.Player1CurrentGamePoints = 3;
+            testMatch.AddPoint(0);
+
+
+            string actualReturn = testMatch.GetMatchResult();
+
+            Assert.Equal(expectedReturn, actualReturn);
+        }
+    }
+}
