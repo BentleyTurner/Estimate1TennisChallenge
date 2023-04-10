b5e28323ab74e7ec67bf1fdecaced15765efeda1
BentleyTurner <bentleyturner@live.com>
2023-04-10 21:30:35 +1000

    Building .NET project

---
+++ Estimate1TennisChallenge/Utils.cs
@@ -0,0 +1,38 @@
+﻿using Estimate1TennisChallenge.Models;
+
+namespace Estimate1TennisChallenge
+{
+    public class Utils
+    {
+        public static bool playerExist(string playerName, List<Player> players)
+        {
+            return players.Any(x => x.Name == playerName);
+        }
+
+        public static Player getPlayer(string playerName, List<Player> players)
+        {
+            return players.Find(x => x.Name == playerName);
+        }
+
+        public static Models.Match getMatch(string matchId, List<Models.Match> matches)
+        {
+            return matches.Find(x => x.Id == matchId);
+        }
+
+        public static String PrintPlayerGames(string playerName, List<Player> players)
+        {
+            Player player = getPlayer(playerName, players);
+            if (player == null) { return $"Couldnt find a player with the name: {playerName}"; }
+            else { return $"{player.GamesWon} {player.GamesLost}"; }
+
+        }
+
+        public static String PrintMatchResult(String matchId, List<Models.Match> matches)
+        {
+            Models.Match matchToPrint = getMatch(matchId, matches);
+            if (matchToPrint == null) { return $"Couldnt find a match with an ID of: {matchId}"; }
+            else { return matchToPrint.GetMatchResult(); }
+        }
+
+    }
+}
