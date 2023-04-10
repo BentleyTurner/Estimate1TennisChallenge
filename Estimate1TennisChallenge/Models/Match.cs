972db8c7465e6611b9c1cea033111fbe4a9d6f2c
BentleyTurner <bentleyturner@live.com>
2023-04-10 21:30:11 +1000

    Adding Match class and Main program

---
+++ Estimate1TennisChallenge/Models/Match.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estimate1TennisChallenge.Models
+{
+    public class Match
+    {
+        public Match() { }
+        public Match(string id, Player player1, Player player2, int player1SetWins, int player2SetWins)
+        {
+            Id = id;
+            Player1 = player1;
+            Player2 = player2;
+            Player1SetWins = player1SetWins;
+            Player2SetWins = player2SetWins;
+
+        }
+        public String? Id { get; set; }
+        public Player? Player1 { get; set; }
+        public Player? Player2 { get; set; }
+
+        public int Player1SetWins = 0;
+        public int Player2SetWins = 0;
+        public int Player1CurrentGamePoints = 0;
+        public int Player2CurrentGamePoints = 0;
+        public int Player1CurrentGameWins = 0;
+        public int Player2CurrentGameWins = 0;
+
+        public void AddPoint(int playerNumber)
+        {
+            if (playerNumber == 0)
+            {
+                Player1CurrentGamePoints++;
+            }
+            else if (playerNumber == 1)
+            {
+                Player2CurrentGamePoints++;
+            }
+            if (Player1GameWinCondition())
+            {
+                Player1CurrentGameWins++;
+                Player1.AddWin();
+                Player2.AddLoss();
+                ResetGameScores();
+            }
+            else if (Player2GameWinCondition())
+            {
+                Player2CurrentGameWins++;
+                Player1.AddLoss();
+                Player2.AddWin();
+                ResetGameScores();
+            }
+            if (Player1CurrentGameWins == 6)
+            {
+                Player1SetWins++;
+                ResetGameWins();
+            }
+            else if (Player2CurrentGameWins == 6)
+            {
+                Player2SetWins++;
+                ResetGameWins();
+            }
+        }
+
+
+        public string GetMatchResult()
+        {
+            string winner = Player1SetWins > Player2SetWins ? Player1.Name : Player2.Name;
+            string loser = winner == Player1.Name ? Player2.Name : Player1.Name;
+            string winnerScore = winner == Player1.Name ? Player1SetWins.ToString() : Player2SetWins.ToString();
+            string loserScore = winner == Player2.Name ? Player1SetWins.ToString() : Player2SetWins.ToString();
+
+            return $"{winner} defeated {loser}\n{winnerScore} to {loserScore}";
+        }
+
+        private bool Player1GameWinCondition()
+        {
+            return (Player1CurrentGamePoints >= 4 && Player1CurrentGamePoints >= Player2CurrentGamePoints + 2);
+        }
+
+        private bool Player2GameWinCondition()
+        {
+            return (Player2CurrentGamePoints >= 4 && Player2CurrentGamePoints >= Player1CurrentGamePoints + 2);
+        }
+        private void ResetGameScores()
+        {
+            Player1CurrentGamePoints = 0;
+            Player2CurrentGamePoints = 0;
+        }
+        private void ResetGameWins()
+        {
+            Player1CurrentGameWins = 0;
+            Player2CurrentGameWins = 0;
+        }
+    }
+}
---
+++ Estimate1TennisChallenge/Program.cs
@@ -0,0 +1,114 @@
+﻿using Estimate1TennisChallenge.Models;
+
+namespace Estimate1TennisChallenge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Read input from text file
+            string[] lines = File.ReadAllLines("C:\\Git\\TennisCalculator\\TennisCalculator\\InputFile.txt");
+            List<Player> Players = new List<Player>();
+
+            // Parse input into list of match results
+            List<Models.Match> Matches = ParseInput(lines, Players);
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(" WELCOME TO TENNIS CALCULATOR ");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Available commands: ");
+            Console.WriteLine("Games Player <Player Name> ");
+            Console.WriteLine("Score Match <Match Id Number> ");
+            Console.WriteLine("Type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    if (input.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Exiting...");
+                        break;
+                    }
+                    else if (input.StartsWith("Games Player", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string[] command = input.Split("Player ", StringSplitOptions.RemoveEmptyEntries);
+                        Console.WriteLine(Utils.PrintPlayerGames(command[1], Players));
+                    }
+                    else if (input.StartsWith("Score Match", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string[] command = input.Split("Match ", StringSplitOptions.RemoveEmptyEntries);
+                        Console.WriteLine(Utils.PrintMatchResult(command[1], Matches));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sorry Couldnt find a match or player with the input provided");
+                }
+
+            }
+        }
+
+        public static List<Models.Match> ParseInput(string[] input, List<Player> players)
+        {
+            List<Models.Match> Matches = new List<Models.Match>();
+            Models.Match currentMatch = new Match();
+
+            foreach (string line in input)
+            {
+                if (line.StartsWith("Match: "))
+                {
+                    currentMatch = new Models.Match();
+                    currentMatch.Id = line.Substring(7);
+                    Matches.Add(currentMatch);
+                }
+                else if (line.Contains(" vs "))
+                {
+                    string[] currentPlayers = line.Split(" vs ");
+
+                    String player1 = currentPlayers[0].Trim();
+                    String player2 = currentPlayers[1].Trim();
+
+                    if (Utils.playerExist(player1, players))
+                    {
+                        currentMatch.Player1 = Utils.getPlayer(player1, players);
+                    }
+                    else
+                    {
+                        players.Add(new Player(player1));
+                        currentMatch.Player1 = Utils.getPlayer(player1, players);
+                    }
+
+                    if (Utils.playerExist(player2, players))
+                    {
+                        currentMatch.Player2 = Utils.getPlayer(player2, players);
+                    }
+                    else
+                    {
+                        players.Add(new Player(player2));
+                        currentMatch.Player2 = Utils.getPlayer(player2, players);
+                    }
+
+                }
+                else if (line != "")
+                {
+                    int playerNumber = int.Parse(line);
+                    currentMatch.AddPoint(playerNumber);
+                }
+            }
+
+            return Matches;
+
+        }
+
+
+    }
+}
